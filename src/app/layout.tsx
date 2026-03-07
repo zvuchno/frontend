f4d50ede6ca7eb6f9262e900aa8d685569677331
Helen <egubanova8@yandex.ru>
2026-03-07 21:28:18 +0300

    feat: add stories

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
-import "./globals.css";
+import "./globals.scss";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
