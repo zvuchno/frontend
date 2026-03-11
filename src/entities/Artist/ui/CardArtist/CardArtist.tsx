5864775c80a3b249ebca038865fb5b599474eeb1
Helen <egubanova8@yandex.ru>
2026-03-11 20:28:09 +0300

    feat: добавлены пропсы компонента и истории

--- src/entities/Artist/ui/CardArtist/CardArtist.tsx
+++ src/entities/Artist/ui/CardArtist/CardArtist.tsx
@@ -1,18 +1,36 @@
-import clsx from "clsx";
 import s from './CardArtist.module.scss';
+import { CardArtistProps } from "./CardArtist.type";
 
-const CardArtist = () => {
+const CardArtist = ({ image, description, onClick }: CardArtistProps) => {
   return (
-    <div className={clsx(s.cardWrapper)}>
-      <div className={clsx(s.card_corner)} />
+    <div className={s.cardWrapper}>
+      <div className={s.cardCorner} />
       <div className={s.card}>
-        {/* <img 
-          className={s.card__image} 
-          src='https://static.vecteezy.com/system/resources/previews/041/486/590/non_2x/children-ponder-the-question-cartoon-characters-girl-and-boy-for-children-design-knowledge-and-education-concept-illustration-vector.jpg'
-        /> */}
+
+        {image && (
+          <img 
+            className={s.card__image} 
+            src={image}
+            alt="Фото артиста"
+          />
+        )}
+
+        {onClick && (
+          <button 
+            className={s.card__button}
+            onClick={onClick}
+          />
+        )}
+
+        {description && (
+          <div className={s.card__description}>
+            <h4 className={s.card__description__title}>{description}</h4>
+          </div>
+        )}
+
       </div>
     </div>
   )
-}
+};
 
 export default CardArtist;
