5674aeb6102da93c13d263d8b32a19f273cd2f89
unknown <lyonya_ivanovskij@mail.ru>
2026-03-30 17:15:50 +0300

    feat: add ArrowIcon

---
+++ src/shared/ui/icons/arrowIcon/ArrowIcon.tsx
@@ -0,0 +1,17 @@
+export const ArrowIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="none">
+    <g
+      stroke="var(--color-text-primary)"
+      stroke-width="1.4"
+      clip-path="url(#a)"
+    >
+      <circle cx="14" cy="14" r="13.3" transform="matrix(0 -1 -1 0 28 28)" />
+      <path stroke-linecap="round" d="m19.602 16.46-5.94-5.939-5.94 5.94" />
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path fill="#fff" d="M28 28V0H0v28z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
