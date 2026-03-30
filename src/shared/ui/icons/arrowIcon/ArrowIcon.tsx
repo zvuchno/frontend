7edc7d87b78e88b79a3d64398ef6aba3ac2eab70
unknown <lyonya_ivanovskij@mail.ru>
2026-03-30 18:07:31 +0300

    fix: Update ArrowIcon

--- src/shared/ui/icons/arrowIcon/ArrowIcon.tsx
+++ src/shared/ui/icons/arrowIcon/ArrowIcon.tsx
@@ -1,12 +1,8 @@
 export const ArrowIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="none">
-    <g
-      stroke="var(--color-text-primary)"
-      stroke-width="1.4"
-      clip-path="url(#a)"
-    >
+    <g stroke="var(--color-text-primary)" strokeWidth="1.4" clipPath="url(#a)">
       <circle cx="14" cy="14" r="13.3" transform="matrix(0 -1 -1 0 28 28)" />
-      <path stroke-linecap="round" d="m19.602 16.46-5.94-5.939-5.94 5.94" />
+      <path strokeLinecap="round" d="m19.602 16.46-5.94-5.939-5.94 5.94" />
     </g>
     <defs>
       <clipPath id="a">
