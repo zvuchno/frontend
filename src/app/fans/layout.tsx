aeb45d97e50259075bb2cf80151c8c54cc79d2ee
Helen <egubanova8@yandex.ru>
2026-04-06 16:43:41 +0300

    feat: добавлен FansLayout

---
+++ src/app/fans/layout.tsx
@@ -0,0 +1,44 @@
+import NavBar from "@/features/profile/ui/NavBar/NavBar";
+import { Title } from "@/shared/ui/Typography/Typography";
+import { AccentContainer } from "@/widgets/layout/ui/accentContainer";
+import s from "./layout.module.scss";
+
+const FansLayout = ({ children }: {children: React.ReactNode}) => {
+
+  const fansProfileRoutes = [
+    {
+      id: 'Профиль',
+      href: '',
+      label: 'Профиль',
+    },
+    {
+      id: 'Избранное',
+      href: '',
+      label: 'Избранное',
+    },
+    {
+      id: 'Заказы',
+      href: '',
+      label: 'Заказы',
+    },
+    {
+      id: 'Релизы',
+      href: '',
+      label: 'Релизы',
+    }
+  ];
+
+  return (
+    <AccentContainer className={s.container}>
+      <Title Tag="h2" className={s.title}>Личный кабинет</Title>
+      <section className={s.section}>
+        <NavBar links={fansProfileRoutes} />
+        <div className={s.section__content}>
+          {children}
+        </div>
+      </section>
+    </AccentContainer>
+  )
+};
+
+export default FansLayout;
