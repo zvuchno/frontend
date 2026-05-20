9eedc590ef828dfe86eb44b58e79308fe60940f8
Helen <egubanova8@yandex.ru>
2026-05-20 14:14:02 +0300

    feat: добавлен Hero компонент для каталога

---
+++ src/app/catalog/page.tsx
@@ -0,0 +1,12 @@
+import { AccentContainer } from "@/widgets/layout/ui/accentContainer"
+import Hero from "./components/hero/Hero"
+
+const CatalogPage = () => {
+  return (
+    <AccentContainer>
+      <Hero />
+    </AccentContainer>
+  )
+}
+
+export default CatalogPage;
