8bc94c8f79a937e8a21b63d42c0749d2f27bf7d2
Helen <egubanova8@yandex.ru>
2026-06-02 17:31:07 +0300

    fix: получение жанров вынесено в лэйаут каталога, доступ через контекст

---
+++ src/app/catalog/provider/FiltersProvider.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { TGenreKind } from "@/api/genresKinds/genresKindsApi.types";
+import { createContext } from "react";
+
+type FiltersContextType = {
+  genresList: TGenreKind[];
+};
+
+export const FiltersContext = createContext<FiltersContextType | undefined>(undefined);
+
+export function FiltersProvider ({ 
+  genresList, 
+  children 
+}: {
+  genresList: TGenreKind[],
+  children: React.ReactNode
+}) {
+
+  const value = {
+    genresList: genresList
+  };
+
+  return (
+    <FiltersContext.Provider value={value}>
+      {children}
+    </FiltersContext.Provider>
+  )
+};
