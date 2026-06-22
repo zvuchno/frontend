1d0925713061a73a909b9e8f6c8f96d1321052b9
Helen <egubanova8@yandex.ru>
2026-06-22 10:25:55 +0300

    feat: add suspense for header

--- src/widgets/layout/Header/ui/AppHeader.tsx
+++ src/widgets/layout/Header/ui/AppHeader.tsx
@@ -1,12 +1,15 @@
 import { DefaultHeaderActions } from "@/widgets/layout/Header/config/headerActions";
 import { AccentContainer } from "@/shared/ui";
 import { HeaderUI } from "./HeaderUI";
+import { Suspense } from "react";
 
 export const AppHeader = () => {
 
   return (
     <AccentContainer>
-      <HeaderUI actions={DefaultHeaderActions} />
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <HeaderUI actions={DefaultHeaderActions} />
+      </Suspense>
     </AccentContainer>
   );
 };
