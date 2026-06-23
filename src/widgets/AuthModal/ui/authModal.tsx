cdf0ead63b308b76fb18033f29c38a7c83295119
Helen <egubanova8@yandex.ru>
2026-06-23 18:52:02 +0300

    fix: handleCloseAuthModal, redirect from VerifySuccess

--- src/widgets/AuthModal/ui/authModal.tsx
+++ src/widgets/AuthModal/ui/authModal.tsx
@@ -2,12 +2,24 @@
 
 import { ModalUI } from "@/shared/ui";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export const AuthModal = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
+  const [isHistoryAvailable, setIsHistoryAvailable] = useState(false);
+
+  useEffect(() => {
+    setIsHistoryAvailable(
+      typeof window !== 'undefined' && 'history' in window
+    );
+  }, []);
 
   const handleCloseModal = () => {
-    router.back();
+    if (isHistoryAvailable && window.history.length > 1) {
+      router.back();
+    } else {
+      router.push('/');
+    }
   };
 
   return (
