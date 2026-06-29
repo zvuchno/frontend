d713d2484fb6c57fb82bc61998366812c3383049
Svetlana <sy.eremeeva@gmail.com>
2026-06-29 16:58:19 -0600

    fix bugs

--- src/shared/utils/formatSum.ts
+++ src/shared/utils/formatSum.ts
@@ -1,10 +1,9 @@
 export const formatSum = (value: string | number): string => {
   const num = typeof value === "string" ? Number(value) : value;
 
-  if (isNaN(num)) return "0.00";
+  if (isNaN(num)) return "0";
   return new Intl.NumberFormat("ru-RU", {
-    maximumFractionDigits: 2,
-    minimumFractionDigits: 2,
+    maximumFractionDigits: 0,
   })
     .format(num)
     .replace(",", ".");
