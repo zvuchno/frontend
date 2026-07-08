89f415098f2ab727a9a6cde175d99d01465ed124
Svetlana <sy.eremeeva@gmail.com>
2026-07-07 21:30:30 -0600

    fix bags

--- src/screens/order/ui/components/OrderDetails/componenents/OrderDeliveryOptions.tsx
+++ src/screens/order/ui/components/OrderDetails/componenents/OrderDeliveryOptions.tsx
@@ -1,16 +1,46 @@
+import { useState } from "react";
 import { useFormContext } from "react-hook-form";
+
+
 
 import { type FieldValues } from "@/screens/order/model/types";
 
+
+
 import { CdekDelivery } from "@/features/CdekDelivery";
+
+
 
 import type { TDeliveryOption } from "@/entities/order";
 
+
+
 import { CheckboxUI } from "@/shared/ui";
+
+
 
 import styles from "../OrderDetails.module.scss";
 import { fieldsConfig } from "../utils";
 import { OrderAddressDetails } from "./OrderAddressDetails";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export const OrderDeliveryOptions = ({
   options,
@@ -21,6 +51,8 @@ export const OrderDeliveryOptions = ({
 }) => {
   const { register, watch } = useFormContext<FieldValues>();
   const currentDeliveryValue = watch("delivery");
+
+  const [hasDeliverySum, setHasDeliverySum] = useState(false);
 
   const deliveryDays = {
     courier: "Доставка займёт 7–21 дней",
@@ -51,7 +83,9 @@ export const OrderDeliveryOptions = ({
         })}
       </div>
       {String(currentDeliveryValue) === "1" && <OrderAddressDetails fieldsDisabled={false} />}
-      {String(currentDeliveryValue) === "2" && <CdekDelivery />}
+      {String(currentDeliveryValue) === "2" && (
+        <CdekDelivery isDeliveryChosen={setHasDeliverySum} />
+      )}
     </section>
   );
 };
