0d280a98ee31faf34db271972c2844b683715e71
Svetlana <sy.eremeeva@gmail.com>
2026-07-15 12:22:25 -0600

    fix reset delivery price without location

--- src/features/CdekDelivery/model/useCdekDeliveryCalculate.tsx
+++ src/features/CdekDelivery/model/useCdekDeliveryCalculate.tsx
@@ -2,11 +2,14 @@ import toast from "react-hot-toast";
 
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
+import { useSelectDeliveryTariff } from "@/entities/order";
+
 import { calculateCdekDelivery } from "../api/cdek.api";
 import type { TCdekData, TCdekPickupDetailsResponse } from "./types";
 
 export function useCdekCalculate() {
   const queryClient = useQueryClient();
+  const { setDeliverySelected } = useSelectDeliveryTariff();
 
   return useMutation<TCdekPickupDetailsResponse, Error, TCdekData>({
     mutationFn: (data: TCdekData) => calculateCdekDelivery(data),
@@ -15,6 +18,7 @@ export function useCdekCalculate() {
     },
     onError: (error) => {
       toast.success(`Ошибка: ${error.message}`);
+      setDeliverySelected({ price: 0 });
     },
   });
 }
