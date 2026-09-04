06a92277694d28f1e31bec5e6c3913fcc55c2e3f
Helen <egubanova8@yandex.ru>
2026-09-04 18:02:24 +0300

    fix: fix createBackendPath

--- src/entities/user/lib/createBackendPath.ts
+++ src/entities/user/lib/createBackendPath.ts
@@ -1,5 +1,4 @@
-//const NO_TRAILING_SLASH_PREFIXES = [""];
-const NO_TRAILING_SLASH_PREFIXES = ["v1/store/me/purchased-music"];
+//const NO_TRAILING_SLASH_PREFIXES = ["v1/store/me/purchased-music"];
 
 export const createBackendPath = (
   path: string[]
@@ -7,11 +6,11 @@ export const createBackendPath = (
   const rawBackendPath = path.join("/");
 
   // Проверяем, попадает ли путь под исключение И при этом НЕ является «детальным» ресурсом
-  const isCollectionPath = NO_TRAILING_SLASH_PREFIXES.some(
-    (prefix) => rawBackendPath === prefix || rawBackendPath.startsWith(`${prefix}/`)
-  );
+  // const isCollectionPath = NO_TRAILING_SLASH_PREFIXES.some(
+  //   (prefix) => rawBackendPath === prefix || rawBackendPath.startsWith(`${prefix}/`)
+  // );
 
-  const backendPath = isCollectionPath && !rawBackendPath.includes('/download-link/track/')
+  const backendPath = !rawBackendPath.includes('/download-link/')
     ? rawBackendPath
     : rawBackendPath.endsWith('/') ? rawBackendPath : `${rawBackendPath}/`;
 
