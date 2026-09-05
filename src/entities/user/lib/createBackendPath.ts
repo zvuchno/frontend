f83647aac2433e989799b19edcd8302c021a6795
Helen <egubanova8@yandex.ru>
2026-09-05 14:27:22 +0300

    fix: add no trailing slash prefix into createBackendPath

--- src/entities/user/lib/createBackendPath.ts
+++ src/entities/user/lib/createBackendPath.ts
@@ -1,24 +1,15 @@
-//const NO_TRAILING_SLASH_PREFIXES = ["v1/store/me/purchased-music"];
+const NO_TRAILING_SLASH_PREFIXES = ["/download-link/", "/cart/me/add"];
 
 export const createBackendPath = (
   path: string[]
 ): Record<"backendPath" | "pathForMatch", string> => {
   const rawBackendPath = path.join("/");
 
-  // Проверяем, попадает ли путь под исключение И при этом НЕ является «детальным» ресурсом
-  // const isCollectionPath = NO_TRAILING_SLASH_PREFIXES.some(
-  //   (prefix) => rawBackendPath === prefix || rawBackendPath.startsWith(`${prefix}/`)
-  // );
-
-  const backendPath = !rawBackendPath.includes('/download-link/')
+  const backendPath = NO_TRAILING_SLASH_PREFIXES.some(
+    (prefix) => rawBackendPath === prefix || rawBackendPath.startsWith(`${prefix}/`)
+  )
     ? rawBackendPath
-    : rawBackendPath.endsWith('/') ? rawBackendPath : `${rawBackendPath}/`;
-
-  // const backendPath = NO_TRAILING_SLASH_PREFIXES.some(
-  //   (prefix) => rawBackendPath === prefix || rawBackendPath.startsWith(`${prefix}/`)
-  // )
-  //   ? rawBackendPath
-  //   : `${rawBackendPath}/`;
+    : `${rawBackendPath}/`;
 
   return {
     backendPath: backendPath,
